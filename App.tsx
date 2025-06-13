4c76891afa8df04b2df7f17940a62619c42c2e2d
boithanh <boithanh01694@gmail.com>
2025-06-13 14:50:16 +0700

    Complete basic layout and main feature of todo app

--- App.tsx
+++ App.tsx
@@ -1,48 +1,113 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, FlatList, Keyboard, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 
+interface ITodo {
+  id: number,
+  name: string
+}
 export default function App() {
-  const [count, setCount] = useState<number>(0);
-  const [name, setName] = useState<string>("");
+  const [todo, setTodo] = useState<string>("hihi");
+  const [listTodo, setListTodo] = useState<ITodo[]>([]);
+
+  const handleAddTodo = () => {
+    if (!todo) {
+      Alert.alert("Bạn chưa nhập todo tên là gì nhé!");
+      return
+    };
+    setListTodo([...listTodo, { id: Math.floor(Math.random() * 2000000), name: todo }]);
+    setTodo("");
+  }
+
+  const deleteTodo = (id: Number) => {
+    const newsTodos = listTodo.filter(item => item.id !== id);
+    setListTodo(newsTodos);
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.formContainer}>
-        <View style={{ marginHorizontal: "auto", width: "100%", marginBottom: 10 }}><Text>Name: {name}</Text></View>
-        <TextInput style={{
-          borderColor: "blue",
-          borderWidth: 1,
-          width: 200,
-          padding: 15,
-          marginBottom: 20
-        }} multiline={true} onChangeText={(value) => { setName(value) }} />
-        <Text style={{ fontSize: 40, fontWeight: "600" }}>Count {count}</Text>
-        <View style={styles.buttonContainer}>
-          <Button title='Increase' onPress={() => { setCount(count + 1) }} />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Todo App</Text>
+        {/* form */}
+        <View style={styles.body}>
+          <TextInput value={todo} autoCapitalize='sentences' style={styles.todoInput} onChangeText={(value) => { setTodo(value) }} />
+          <Button title='Add todo' onPress={handleAddTodo} />
         </View>
-        <StatusBar style="auto" />
+        {/* list todo */}
+        <View style={styles.todoContainer}>
+          {/* keyExtractor để khai báo key prop tranh bị giảm hiệu suât khi react render */}
+          <FlatList
+            data={listTodo}
+            keyExtractor={item => item.id + ""}
+            renderItem={({ item }) => {
+              return (
+
+                <Pressable onPress={() => {
+                  Alert.alert(`Bạn đang xóa mục: ${item.name}`, 'chắc chắn xóa không?', [
+                    {
+                      text: 'Hủy',
+                      style: 'cancel',
+                    },
+                    {
+                      text: 'Xác nhận',
+                      onPress: () => deleteTodo(item.id)
+                    },
+                  ])
+                }}>
+                  <Text style={styles.todoItem} key={item.id} >{item.name}</Text>
+                </Pressable>
+                //Cách cũ không khuyến khích sử dụng bởi React, tương lai nên migrate sang component Pressable để tránh lỗi phat sinh do TouchableOpacity không còn được hỗ trợ nửa
+
+                // <TouchableOpacity onPress={() => {
+                //   deleteTodo(item.id);
+                //   alert("Ban vua xoa todo co ma: " + item.id);  
+                // }}>
+                //   <Text style={styles.todoItem} key={item.id} >{item.name}</Text>
+                // </TouchableOpacity>
+              )
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E0DFD5',
+    paddingTop: 50
   },
-  buttonContainer: {
-    marginTop: 20
+  header: {
+    backgroundColor: "#EF6461",
+    paddingHorizontal: 20,
+    textAlign: "center",
+    fontSize: 40,
+    color: "#313638"
   },
-  formContainer: {
-    borderColor: "hotpink",
+  todoInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#313638",
+    padding: 5,
+    margin: 15
+
+  },
+  todoContainer: {
+    paddingHorizontal: 20
+  },
+  todoItem: {
+    fontSize: 20,
     borderWidth: 1,
-    width: "80%",
-    paddingHorizontal: 40,
-    paddingVertical: 80,
-    justifyContent: "center",
-    alignItems: "center"
+    marginBottom: 10,
+    borderStyle: "dashed",
+    padding: 10,
+    backgroundColor: "#E4B363",
+    borderColor: "#313638",
+    color: "#313638"
+  },
+  body: {
+    paddingHorizontal: 20,
+    marginBottom: 20
   }
 });
